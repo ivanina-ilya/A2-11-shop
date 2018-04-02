9c361d6de43ad78f674cac24fd17b4a22743612f
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-04-02 11:42:56 +0300

    ConstantsServiceDemo

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,28 +1,21 @@
-import {InjectionToken, NgModule} from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ConfigOptionsService} from "../config/config-options.service";
-import {RandomDemoFactory, RandomStringFactory} from "./random-demo.factory";
-import {DemoComponent} from "./demo.component";
+import { ConfigOptionsService } from '../config/config-options.service';
+import { RandomDemoFactory, RandomStringFactory } from './random-demo.factory';
+import { DemoComponent } from './demo.component';
 import { DemoDirective } from './demo.directive';
-import {SharedModule} from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
 
+import { ConstantsServiceProvider } from './constants.service';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule
-  ],
-  exports: [
-    DemoComponent
-  ],
-  declarations: [
-    DemoComponent,
-    DemoDirective
-  ],
+  imports: [CommonModule, SharedModule],
+  exports: [DemoComponent],
+  declarations: [DemoComponent, DemoDirective],
   providers: [
     ConfigOptionsService,
-
-    {provide: RandomStringFactory, useFactory: RandomDemoFactory(8)}
+    ConstantsServiceProvider,
+    { provide: RandomStringFactory, useFactory: RandomDemoFactory(8) }
   ]
 })
-export class DemoModule { }
+export class DemoModule {}
